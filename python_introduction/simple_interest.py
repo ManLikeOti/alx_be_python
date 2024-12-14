9abb212b56c298a9f52d211b3bd4fbdfc8dac102
ManLikeOti <anigboguotuto@gmail.com>
2024-12-14 21:39:03 +0100

    updated code lines

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,9 +1,5 @@
 principal = 1000
 rate = 0.05
 time = 3
-P = principal
-R = rate
-T = time
-interest = P * R * T
-I = interest
-print("The simple interst is:", I )
+interest = principal * rate * time
+print("The simple interst is:", interest )

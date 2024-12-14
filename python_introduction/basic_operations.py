f74412129895c7b67c3ac2feaf001b4757911a27
ManLikeOti <anigboguotuto@gmail.com>
2024-12-14 21:11:59 +0100

    changes to files

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,4 +1,8 @@
 number1 = int(10)
 number2 = int(5)
 number1_plus_number2 = number1 + number2
-print("addition of", number1, "and", number2, "is", number1_plus_number2 )
+number2_minus_number1 = number1 - number2
+number1_times_number2 = number1 * number2
+print("Addition of", number1, "and", number2, "is", number1_plus_number2 )
+print("Subtraction of", number1, "and", number2, "is", number2_minus_number1 )
+print("Multiplication of", number1, "and", number2, "is", number1_times_number2 )

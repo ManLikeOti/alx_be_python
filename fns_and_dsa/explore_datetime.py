662ff41bd5277dbacab9ccfdbbc6f152c35227a9
ManLikeOti <anigboguotuto@gmail.com>
2025-01-12 23:59:56 +0100

    new files

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,2 +1,5 @@
-def display_current_datetime()
-    
+import datetime
+def display_current_datetime():
+    current_date = datetime.datetime.now()
+    print(current_date)
+input = int("Enter the nuber of days to add to the current date:")
--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,1 +1,5 @@
-def convert_to_celcius(fahrenheit)
+FAHRENHEIT_TO_CELSIUS_FACTOR = (5/9)
+CELSIUS_TO_FAHRENHEIT_FACTOR = (9/5)
+
+def convert_to_celcius(fahrenheit):
+    

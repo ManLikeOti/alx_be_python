fae8cd36ad8f37dad864afb11cf020ac71809354
ManLikeOti <anigboguotuto@gmail.com>
2025-01-19 22:46:34 +0100

    oop python

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -2,4 +2,4 @@ import datetime
 def display_current_datetime():
     current_date = datetime.datetime.now()
     print(current_date)
-input = int("Enter the nuber of days to add to the current date:")
+input(int("Enter the nuber of days to add to the current date:"))

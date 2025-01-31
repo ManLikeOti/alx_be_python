dfd9341759516510cd823540482f659a7fe27348
ManLikeOti <anigboguotuto@gmail.com>
2025-01-31 17:24:37 +0100

    update to alx files

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,13 +1,20 @@
 def perform_operation(num1, num2, operation):
-    match operation:
-        case "add":
-            return num1 + num2
-        case "subtract":
-            return num1 - num2
-        case "multiply":
-            return num1 * num2
-        case "divide":
-            if num2 == int("0"):
-                return "Cannot divide by zero."
-            else:
-                return num1 / num2
+    #"""Accepts numbers as input and perform arithmetric operation with (add, subtract, divide and multiply, then return a message to the user if divisor is 0)"""
+    if operation == 'add':
+        num1 + num2
+        result = num1 + num2
+        return f"Result: {result}"
+    elif operation == 'subtract':
+        num1 - num2
+        result = num1 + num2
+        return f"Result: {result}"
+    elif operation == 'multiply':
+        result = num1 * num2
+        return f"Result: {result}"
+    elif operation == 'divide':
+        if num2 == 0:
+            result = f"{num1} cannot be divided by 0"
+            return result
+        else:
+            result = num1 / num2
+            return f"Result: {result}"
--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -5,29 +5,42 @@ def display_menu():
     print("2. Remove Item")
     print("3. View List")
     print("4. Exit")
-def main():
-    shopping_list = []
-    while True:
-        display_menu()
-        choice = input("Enter your choice: ")
 
-        if choice == '1':
+def check_empty_list(shopping_list_menu):
+    if shopping_list_menu == []:
+        print("Shopping list is empty!")
+        return True
+    return False
 
-            # Prompt for and add an item
+while True:
+    display_menu()
+    choice = input("Enter your choice: ")
+    if choice == '1':
+        # Prompt for and add an item
+        item_name = input("Enter the item to add: ").lower().strip()
+        shopping_list.append(item_name)
+        print(item_name, "has been added to shopping list successfully.")
+    elif choice == '2':
+        # Prompt for and remove an item
+        if check_empty_list(shopping_list):
             pass
-        elif choice == '2':
-
-            # Prompt for and remove an item
-            pass
-        elif choice == '3':
-
-            # Display the shopping list
-            pass
-        elif choice == '4':
-            print("Goodbye!")
-            break
         else:
-            print("Invalid choice. Please try again.")
-
-if __name__ == "__main__":
-    main()
+            item_name = input("Enter item to remove: ").lower().strip()
+            if item_name in shopping_list:
+              shopping_list.remove(item_name)
+              print(item_name, "has been removed from shopping list successfully.")
+            else:
+                print("Item not found in list!")
+    elif choice == '3':
+        # Display the shopping list
+        if check_empty_list(shopping_list):
+            pass
+        else:
+            for item in shopping_list:
+                print(item)
+    elif choice == '4':
+        print("Goodbye!")
+        break
+    else:
+        print("Invalid choice. Please try again.")
+        
